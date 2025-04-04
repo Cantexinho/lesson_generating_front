6beeb33eb0463db6eddf59636167dc71f7e390ea
Cantexinho <pk.developing@gmail.com>
2025-04-04 09:30:34 +0300

    Added dropdown-hover-area

--- src/components/Home/NavBarButton.js
+++ src/components/Home/NavBarButton.js
@@ -3,38 +3,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { useSelector } from "react-redux";
 import { selectTheme } from "../../redux/themeSlice";
+import { useNavigate } from "react-router-dom";
 
 const NavBarButton = ({ label, showArrow, subpages }) => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef(null);
   const theme = useSelector(selectTheme);
+  const navigate = useNavigate();
 
   const handleButtonClick = () => {
-    setDropdownOpen(!isDropdownOpen);
+    navigate("/company");
   };
 
-  const handleClickOutside = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setDropdownOpen(false);
-    }
+  const handleMouseEnter = () => {
+    setDropdownOpen(true);
   };
 
-  useEffect(() => {
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, []);
+  const handleMouseLeave = () => {
+    setDropdownOpen(false);
+  };
 
   return (
     <li
       ref={dropdownRef}
-      className={`relative flex items-center justify-center p-2 h-10 text-md rounded-lg ${
+      className={`relative flex items-center justify-center p-2 h-10 text-md rounded-lg  hover:scale-110 ${
         theme.isDarkTheme
           ? "text-white hover:bg-gray-800"
           : "text-black hover:bg-gray-100"
       }`}
       onClick={handleButtonClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       {label}
       {showArrow && (
@@ -42,13 +41,14 @@ const NavBarButton = ({ label, showArrow, subpages }) => {
       )}
       {isDropdownOpen && showArrow && (
         <ul
-          className={`absolute top-full border rounded mt-1 ${
+          className={`absolute top-full left-0 border rounded mt-1 ${
             theme.isDarkTheme ? "border-gray-600" : "border-gray-200"
           }`}
         >
+          <div className="dropdown-hover-area" />
           {subpages.map((subpage) => (
             <li
-              className={`flex items-center justify-center text-base w-24 h-10 ${
+              className={`flex items-center justify-center text-base w-32 h-10 ${
                 theme.isDarkTheme
                   ? "dark-second-bg text-white hover:bg-gray-800"
                   : "light-second-bg text-black hover:bg-gray-100"
