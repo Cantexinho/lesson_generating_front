142435ebdc5c32b4db7d8eee496bc9bda8aacc08
Cantexinho <pk.developing@gmail.com>
2025-04-09 14:01:02 +0300

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/Home/NavBar";
-import HomeSection from "../components/Home/HomeSection";
+import ImageSection from "../components/Sections/ImageSection";
 import CustomFooter from "../components/Footers/CustomFooter";
+import TopSection from "../components/Sections/TopSection";
 import { useSelector } from "react-redux";
 import { selectTheme } from "../redux/themeSlice";
 
 const Home = () => {
   const theme = useSelector(selectTheme);
+  const bgImage = require("../assets/images/ai_rome_1.png");
   return (
     <div
       className={`flex flex-col min-h-screen ${
@@ -14,29 +16,39 @@ const Home = () => {
       }`}
     >
       <NavBar />
-      <div className="flex-grow flex flex-col pt-10 mt-10 ">
-        <HomeSection
-          img_placement={"right"}
-          img_name={"ai_learning"}
-          image_props={"w-72"}
+      <div className="flex-grow flex flex-col">
+        <TopSection />
+        <ImageSection
+          imgPlacement={"right"}
+          imgName={"ai_presenter_1"}
+          sectionProps={
+            "flex items-center justify-center gap-8 p-8 lg:px-24 xl:px-32 w-full bg-cover bg-center"
+          }
+          imageProps={"w-72"}
           sectionHeader={"Gather information quickly!"}
           sectionText={
             "In the fast-paced world of today, the ability to gather information quickly is paramount. In the times of information, Artificial Intelligence has emerged as a game-changer, revolutionizing the speed and efficiency with which we can access and process information."
           }
         />
-        <HomeSection
-          img_placement={"left"}
-          img_name={"student_learning"}
-          image_props={"w-72"}
+        <ImageSection
+          imgPlacement={"left"}
+          imgName={"student_learning"}
+          sectionProps={
+            "flex items-center justify-center gap-8 p-8 lg:px-24 xl:px-32 w-full "
+          }
+          imageProps={"w-72"}
           sectionHeader={"Learn efficiently!"}
           sectionText={
             "AI-driven tools excel at predictive analysis, anticipating user needs and proactively providing information before it's explicitly requested. These systems can learn from user behavior, preferences, and historical data to tailor their outputs, creating a personalized and streamlined information retrieval experience."
           }
         />
-        <HomeSection
-          img_placement={"right"}
-          img_name={"futuristic_future"}
-          image_props={"w-72"}
+        <ImageSection
+          imgPlacement={"right"}
+          imgName={"futuristic_future"}
+          sectionProps={
+            "flex items-center justify-center gap-8 p-8 lg:px-24 xl:px-32 w-full "
+          }
+          imageProps={"w-72"}
           sectionHeader={"Move to a new beginning!"}
           sectionText={
             "AI is transforming the way we access and utilize data and promises a future where the quest for information is not just quick but also exceptionally insightful."
