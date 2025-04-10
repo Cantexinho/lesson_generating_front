b4dc1bdfae1c4fefe186fada27b19f44c024855b
Cantexinho <pk.developing@gmail.com>
2025-04-10 19:29:12 +0300

    Reformated Navbar Menu Settings and Playground buttons

--- src/components/Home/NavBarDropdown.js
+++ src/components/Home/NavBarDropdown.js
@@ -2,7 +2,7 @@ const NavBarDropdown = ({ subpages }) => {
   const numCols = Object.keys(subpages).length;
 
   return (
-    <ul className="absolute z-0 top-full left-0 border rounded-xl px-4 py-1 mt-4 min-w-max bg-transparent-light dark:bg-transparent-dark border-gray-200 dark:border-gray-600">
+    <ul className="absolute z-0 top-full left-0 border rounded-xl px-4 py-1 mt-3 min-w-max bg-transparent-light dark:bg-transparent-dark border-gray-200 dark:border-gray-600">
       <div className="dropdown-hover-area" />
       <div className={`grid grid-cols-${numCols} gap-2`}>
         {Object.entries(subpages).map(([column, items]) => (
--- src/components/Home/NavBarMenu.js
+++ src/components/Home/NavBarMenu.js
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import ThemeButton from "../Global/ThemeButton";
 
 const NavBarMenu = ({ label, showArrow, subpages }) => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -27,7 +26,7 @@ const NavBarMenu = ({ label, showArrow, subpages }) => {
   return (
     <li
       ref={dropdownRef}
-      className="absolute flex items-center justify-center rounded-full right-8 w-28 my-2 h-8 text-md border lg:hidden bg-transparent-light dark:bg-transparent-dark text-black dark:text-white border-gray-600 dark:border-white  hover:bg-gray-300 dark:hover:bg-gray-800"
+      className="relative flex items-center justify-center rounded-full w-28 h-8 text-md border bg-transparent-light dark:bg-transparent-dark text-black dark:text-white border-gray-600 dark:border-white hover:bg-gray-300 dark:hover:bg-gray-800"
       onClick={handleButtonClick}
     >
       <span className="font-semibold font-custom ">{label}</span>
@@ -35,13 +34,12 @@ const NavBarMenu = ({ label, showArrow, subpages }) => {
         <FontAwesomeIcon icon={faArrowDown} className="ml-2" size="xs" />
       )}
       {isDropdownOpen && showArrow && (
-        <ul className="absolute top-full bg-white border border-gray-400 rounded mt-1 bg-transparent-light dark:bg-transparent-dark">
+        <ul className="absolute top-full bg-white border border-gray-400 mt-1 bg-transparent-light dark:bg-transparent-dark">
           {subpages.map((subpage) => (
             <li className="flex items-center justify-center text-base w-32 h-10 bg-transparent-light dark:bg-transparent-dark text-black dark:text-white hover:bg-gray-300 dark:hover:bg-gray-900">
               <a href={subpage}>{subpage}</a>
             </li>
           ))}
-          <ThemeButton passed_props={"my-3 ml-4"} />
         </ul>
       )}
     </li>
