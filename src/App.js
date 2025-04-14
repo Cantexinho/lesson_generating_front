b1c108850bb97c8f040e6a3908a822add18a63fc
Cantexinho <pk.developing@gmail.com>
2025-04-14 20:47:29 +0300

    Added article routing

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Home from "./pages/Home";
 import PostLogin from "./pages/PostLogin";
 import Support from "./pages/Support";
 import Business from "./pages/Business";
+import NewsPage from "./pages/News";
+import ArticlePage from "./pages/Article";
 import { useSyncDarkMode } from "./hooks/useSyncDarkMode";
 import { trackPageView } from "./analytics";
 import { useEffect } from "react";
@@ -32,6 +34,8 @@ function App() {
       <Route exact path="/register" element={<Register />} />
       <Route exact path="/support" element={<Support />} />
       <Route exact path="/business" element={<Business />} />
+      <Route path="/news" element={<NewsPage />} />
+      <Route path="/news/:id" element={<ArticlePage />} />
       <Route path="/home" element={<Home />} />
       <Route path="/" element={<Home />} />
       <Route path="/playground" element={<Playground />} />
