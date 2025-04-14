7e619133ed1acbcd435324fb2a37e7d2468beb23
Cantexinho <pk.developing@gmail.com>
2025-04-14 20:44:49 +0300

    Updated analytics with correct env value

--- src/analytics.js
+++ src/analytics.js
@@ -1,6 +1,6 @@
 import ReactGA from "react-ga4";
 
-const ga_measurement_id = process.env.GA_MEASUREMENT_ID;
+const ga_measurement_id = process.env.REACT_APP_GA_MEASUREMENT_ID;
 ReactGA.initialize(ga_measurement_id);
 
 export const trackPageView = () => {
